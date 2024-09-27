cb17b59ba4879d49ce4a65c996db74f78b7861d9
RaychuW <rb.alhamdan@gmail.com>
2024-09-28 00:35:43 +0300

    login logout

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from openai import OpenAI
-import anthropic
-from flask import Flask, render_template, request, redirect, session, url_for, Response
+from flask import Flask, render_template, session, Response, request, redirect, flash
+from gevent.pywsgi import WSGIServer
+from anthropic import Anthropic
 from dotenv import load_dotenv
+from openai import OpenAI
 from os import getenv
 import sse
-from gevent.pywsgi import WSGIServer
-import time
 
 load_dotenv()
 
@@ -14,32 +13,56 @@ app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 
 channel = sse.Channel()
 
-
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index():
-    return render_template("conversation.html")
+    return redirect("home")
 
-@app.route('/subscribe')
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/subscribe")
 def subscribe():
     return channel.subscribe()
 
-@app.route('/publish', methods=["POST"])
+@app.route("/publish", methods=["POST"])
 def publish():
-    # time.sleep(.5)
-    channel.publish('message here') # this will be returned to client
+    channel.publish("GPT-4o:chatgpt response") # this will be returned to client
+    channel.publish("Sonnet:claude response")
+    channel.publish("Opus:claude response")
+    # chatgpt = openai("chatgpt:" + request.data.decode())
+    # channel.publish(chatgpt)
     return Response(status=200)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username == "rayan" and password == "pass":
+            session["username"] = username
+            return redirect("home")
+        else:
+            flash("Login failed")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect("home")
 
 def openai(prompt):
     chatGPT_client = OpenAI()
     response = chatGPT_client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}],
-        max_tokens=1000
+        max_tokens=1000,
+        temperature=0
     )
-    return response
+    return response.choices[0].message.content
 
 def anthropic(prompt):
-    claude_client = anthropic.Anthropic()
+    claude_client = Anthropic()
     response = claude_client.messages.create(
         model="claude-3-5-sonnet-20240620",
         max_tokens=1000,
@@ -48,20 +71,14 @@ def anthropic(prompt):
         messages=[
             {
                 "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": prompt
-                    }
-                ]
+                "content": [{"type": "text", "text": prompt}]
             }
         ]
     )
+    print(response) 
     return response
 
 if __name__ == "__main__":
     server = WSGIServer(("127.0.0.1", 5000), app)
     print("Server started at 127.0.0.1:5000")
     server.serve_forever()
-    # response = openai("say one random word")
-    # response_message = response.choices[0].message.content
