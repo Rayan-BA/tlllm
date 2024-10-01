7ab8f6dcb8b6bfffb3de7918805636f038032324
RaychuW <rb.alhamdan@gmail.com>
2024-10-01 19:51:37 +0300

    prompt saving and processing

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, session, Response, request, redirect, flash
+from flask import Flask, render_template, session, Response, request, redirect, make_response
+from flask_socketio import SocketIO, emit
 from gevent.pywsgi import WSGIServer
 from flask_migrate import Migrate
 from anthropic import Anthropic
 from dotenv import load_dotenv
+from datetime import timedelta
 from openai import OpenAI
 from os import getenv
 from db import *
-from flask_socketio import SocketIO, emit
+import json
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
+app.permanent_session_lifetime = timedelta(hours=1)
 db.init_app(app)
 
 migrate = Migrate(app, db, render_as_batch=True)
@@ -20,21 +23,25 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return redirect("home")
+    return redirect("/home")
 
 @app.route("/home")
 def home():
+    chats = []
     if session.get("username"):
         user = User.query.filter(User.username == session.get("username")).first()
-        chats = Chat.query.filter(Chat.user_id == user.id).all()
+        if session.get("user_id"):
+            chats = Chat.query.filter(Chat.user_id == user.id).all()
     return render_template("home.html", chats=chats)
 
-@app.route("/home?chat=<chat_id>")
+@app.route("/home/<chat_id>")
 def get_chat(chat_id):
-    # chat = Chat.query.get(chat_id)
-    print(chat_id)
+    if db.session.get(Chat, chat_id).user_id != session.get("user_id"):
+        return redirect("/home")
+        
+    chats = Chat.query.filter(Chat.user_id == session.get("user_id")).all()
     messages = Message.query.filter(Message.chat_id == chat_id).order_by(Message.timestamp.desc()).all()
-    return render_template("home.html", chat_msgs=messages)
+    return render_template("home.html", chats=chats, chat_msgs=messages)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -44,7 +51,10 @@ def login():
         userMatch = User.query.filter(User.username == username).first()
         if userMatch and userMatch.check_password(password):
             session["username"] = username
-            return redirect("home")
+            session["user_id"] = userMatch.id
+            res = make_response(redirect("/home"))
+            res.set_cookie("username", username)
+            return res
         else:
             print("login failed")
             return redirect("login")
@@ -53,12 +63,13 @@ def login():
 @app.route("/logout")
 def logout():
     session.pop("username", None)
-    return redirect("home")
+    session.pop("user_id", None)
+    return redirect("/home")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if session.get("username"):
-        return redirect("home")
+        return redirect("/home")
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -68,31 +79,67 @@ def register():
             newUser = User(username, password)
             db.session.add(newUser)
             db.session.commit()
-            session["username"] = username
-            return redirect("home")
+            session["username"] = newUser.username
+            session["user_id"] = newUser.id
+            return redirect("/home")
 
     return render_template("register.html")
 
-@socketio.on("user message")
+@socketio.on("user prompt")
 def client(msg):
     username = session.get("username")
-    if username:
-        gpt_msg = openai(msg.get("content"))
-        user = User.query.filter(User.username == username).first()
-        if msg.get("chat_id"):
-            chat = db.session.get(Chat, int(msg.get("chat_id")))
-            user_message = Message(content=msg.get("content"), user=user, chat=chat)
-            gpt_message = Message(content=gpt_msg, chat=chat)
-        else:
-            chat = Chat(user=user)
-            user_message = Message(content=msg.get("content"), user=user, chat=chat)
-            gpt_message = Message(content=gpt_msg, chat=chat)
 
-        # db.session.add_all([user_message, gpt_message])
-        db.session.add(user_message)
-        db.session.add(gpt_message)
+    if username is None:
+        print("go back")
+        return Response(status=401)
+
+    response = openai(msg.get("content"))
+    model = response.model
+    gpt_msg = response.choices[0].message.content
+
+    if User.query.filter(User.username == model).first() is None:
+        newModel = User(model, None, True)
+        db.session.add(newModel)
         db.session.commit()
-        emit("llm message", "GPT-4o:" + gpt_msg)
+        session["model_id"] = newModel.id
+
+    # if username:
+    user = User.query.filter(User.username == username).first()
+    model = User.query.filter(User.is_llm).first()
+    # TODO: handle fresh chat
+    if msg.get("chat_id") == "home":
+        chat = Chat(user=user)
+    else:
+        chat = db.session.get(Chat, int(msg.get("chat_id")))
+
+    user_message = Message(content=msg.get("content"), user=user, chat=chat)
+    gpt_message = Message(content=gpt_msg, chat=chat, user=model)
+
+
+    user_message_dict = user_message.__dict__.copy()
+    user_message_dict["user"] = user_message_dict.get("user").__dict__.copy()
+    del user_message_dict["user"]["password"]
+    del user_message_dict["user"]["_sa_instance_state"]
+    del user_message_dict["_sa_instance_state"]
+    del user_message_dict["chat"]
+
+    gpt_message_dict = gpt_message.__dict__.copy()
+    gpt_message_dict["user"] = gpt_message_dict.get("user").__dict__.copy()
+    del gpt_message_dict["user"]["password"]
+    del gpt_message_dict["user"]["_sa_instance_state"]
+    del gpt_message_dict["_sa_instance_state"]
+    del gpt_message_dict["chat"]
+
+    db.session.add(user_message)
+    db.session.add(gpt_message)
+    db.session.commit()
+
+    # if msg.get("chat_id") == "home":
+    #     return redirect(f"/home/{chat.id}")
+    # else:
+        # emit("llm message", gpt_message)
+
+    emit("llm message", gpt_message_dict)
 
 def openai(prompt):
     chatGPT_client = OpenAI()
@@ -102,7 +149,7 @@ def openai(prompt):
         max_tokens=1000,
         temperature=0
     )
-    return response.choices[0].message.content
+    return response
 
 def anthropic(prompt):
     claude_client = Anthropic()
