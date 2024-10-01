571ea6d79a1b18ebe18a2075d18c54e17933ea41
RaychuW <rb.alhamdan@gmail.com>
2024-10-01 19:50:34 +0300

    new column

--- db.py
+++ db.py
@@ -4,9 +4,15 @@ import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
+    def __init__(self, username, password, is_llm=False):
+        self.username = username
+        self.password = password
+        self.is_llm = is_llm
+
     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     username = db.Column(db.String(32), unique=True)
-    password = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String(32))
+    is_llm = db.Column(db.Boolean, nullable=False)
 
     chats = db.relationship("Chat", backref = "user", cascade = "all, delete-orphan", lazy = "dynamic")#, primaryjoin="User.id == Chat.user_id")
     messages = db.relationship("Message", backref = "user", cascade = "all, delete-orphan", lazy = "dynamic")#, primaryjoin="User.id == Chat.user_id")
